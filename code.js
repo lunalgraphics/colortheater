d34ed55e654df1786f7fb5e700fae1f646ac16ce
Yikuan Sun <yikuansun2015@gmail.com>
2024-07-02 13:58:08 -0400

    Presets are back!
    Nice

--- code.js
+++ code.js
@@ -175,3 +175,115 @@ if ((new URLSearchParams(location.search)).get("portal") == "photopea") {
         });
     });
 }
+
+document.querySelector("#exportPresetBtn").addEventListener("click", () => {
+    let xDoc = document.implementation.createDocument(null, "ctpreset");
+    let root = xDoc.querySelector("ctpreset");
+    let basicAdjNode = xDoc.createElement("basicAdj");
+    basicAdjNode.setAttribute("brightness", basicAdj["brightness"] * 100);
+    basicAdjNode.setAttribute("contrast", basicAdj["contrast"] * 100);
+    basicAdjNode.setAttribute("saturate", basicAdj["saturate"] * 100);
+    basicAdjNode.setAttribute("sepia", basicAdj["sepia"] * 100);
+    root.appendChild(basicAdjNode);
+    let splitToningNode = xDoc.createElement("splitToning");
+    splitToningNode.setAttribute("hColor", document.querySelector("#toningHColorCtrl").value);
+    splitToningNode.setAttribute("hAmnt", parseFloat(document.querySelector("#toningHAmntCtrl").value));
+    splitToningNode.setAttribute("sColor", document.querySelector("#toningSColorCtrl").value);
+    splitToningNode.setAttribute("sAmnt", parseFloat(document.querySelector("#toningSAmntCtrl").value));
+    root.appendChild(splitToningNode);
+    let cMatrixNode = xDoc.createElement("cMatrix");
+    for (let row = 0; row < 4; row++) {
+        for (let col = 0; col < 5; col++) {
+            let cellNode = xDoc.createElement("cell");
+            let cellId = row + "" + col;
+            cellNode.setAttribute("id", "cell" + cellId);
+            cellNode.innerHTML = document.querySelector("#cmatrixcell" + cellId + " input[type=number]").value;
+            cMatrixNode.appendChild(cellNode);
+        }
+    }
+    root.appendChild(cMatrixNode);
+    let vignetteNode = xDoc.createElement("vignette");
+    vignetteNode.setAttribute("color", document.querySelector("#vignettecolorcontrol").value);
+    vignetteNode.setAttribute("scale", document.querySelector("#vignettescalecontrol").value);
+    vignetteNode.setAttribute("fill", document.querySelector("#vignettefillcontrol").value);
+    vignetteNode.setAttribute("blending", document.querySelector("#vignetteBlendingCtrl").value);
+    root.appendChild(vignetteNode);
+    let tintNode = xDoc.createElement("tint");
+    tintNode.setAttribute("color", document.querySelector("#tintcolorcontrol").value);
+    tintNode.setAttribute("fill", document.querySelector("#tintfillcontrol").value);
+    root.appendChild(tintNode);
+    let cereal = new XMLSerializer();
+    let output = cereal.serializeToString(xDoc);
+    let blobFish = new Blob([ output ], { type: "application/ctxml" });
+    let a = document.createElement("a");
+    a.href = URL.createObjectURL(blobFish);
+    a.download = "preset.ctxml";
+    a.click();
+});
+
+document.querySelector("#importPresetBtn").addEventListener("click", () => {
+    let fileUpload = document.createElement("input");
+    fileUpload.type = "file";
+    fileUpload.accept = "application/ctxml";
+    fileUpload.addEventListener("change", (e) => {
+        let file = e.target.files[0];
+        let fR = new FileReader();
+        fR.addEventListener("loadend", (e2) => {
+            let text = e2.target.result;
+            let parser = new DOMParser();
+            let xDoc = parser.parseFromString(text, "text/xml");
+            let basicAdjNode = xDoc.querySelector("basicAdj");
+            document.querySelector("#brightnessCtrl").value = basicAdjNode.getAttribute("brightness");
+            basicAdj["brightness"] = parseFloat(document.querySelector("#brightnessCtrl").value) / 100;
+            document.querySelector("#contrastCtrl").value = basicAdjNode.getAttribute("contrast");
+            basicAdj["contrast"] = parseFloat(document.querySelector("#contrastCtrl").value) / 100;
+            document.querySelector("#saturationCtrl").value = basicAdjNode.getAttribute("saturate");
+            basicAdj["saturate"] = parseFloat(document.querySelector("#saturationCtrl").value) / 100;
+            document.querySelector("#sepiaCtrl").value = basicAdjNode.getAttribute("sepia");
+            basicAdj["sepia"] = parseFloat(document.querySelector("#sepiaCtrl").value) / 100;
+            applyBasicAdj();
+            let splitToningNode = xDoc.querySelector("splitToning");
+            document.querySelector("#toningHColorCtrl").value = splitToningNode.getAttribute("hColor");
+            document.querySelector("#toningHAmntCtrl").value = splitToningNode.getAttribute("hAmnt");
+            document.querySelector("#toningSColorCtrl").value = splitToningNode.getAttribute("sColor");
+            document.querySelector("#toningSAmntCtrl").value = splitToningNode.getAttribute("sAmnt");
+            updateSplitToning();
+            for (let row = 0; row < 4; row++) {
+                for (let col = 0; col < 5; col++) {
+                    let cellId = row + "" + col;
+                    let cellNode = xDoc.querySelector("cMatrix #cell" + cellId);
+                    document.querySelector("#cmatrixcell" + cellId + " input[type=number]").value = cellNode.innerHTML;
+                    colorMatrixValues[row][col] = parseFloat(cellNode.innerHTML) / 100;
+                }
+            }
+            document.querySelector("#colorgrade feColorMatrix").setAttribute("values", arraytostring(colorMatrixValues));
+            let vignetteNode = xDoc.querySelector("vignette");
+            document.querySelector("#vignettecolorcontrol").value = vignetteNode.getAttribute("color");
+            document.querySelector("#vignettescalecontrol").value = vignetteNode.getAttribute("scale");
+            document.querySelector("#vignettefillcontrol").value = vignetteNode.getAttribute("fill");
+            document.querySelector("#vignetteBlendingCtrl").value = vignetteNode.getAttribute("blending");
+            document.querySelectorAll("#vignetteGradient stop")[0].setAttribute("offset", (100 - parseFloat(document.querySelector("#vignettescalecontrol").value)).toString() + "%");
+            document.querySelector("#vignetteRect").setAttribute("fill-opacity", document.querySelector("#vignettefillcontrol").value / 100);
+            document.querySelectorAll("#vignetteGradient stop")[1].style.stopColor = document.querySelector("#vignettecolorcontrol").value;
+            document.querySelector("#vignetteRect").style.mixBlendMode = document.querySelector("#vignetteBlendingCtrl").value;
+            let tintNode = xDoc.querySelector("tint");
+            document.querySelector("#tintcolorcontrol").value = tintNode.getAttribute("color");
+            document.querySelector("#tintfillcontrol").value = tintNode.getAttribute("fill");
+            document.querySelector("#tintlayer").style.fill = document.querySelector("#tintfillcontrol").value;
+            document.querySelector("#tintlayerXtra").style.fill = document.querySelector("#tintfillcontrol").value;
+            document.querySelector("#tintlayer").setAttribute("fill-opacity", parseFloat(document.querySelector("#tintfillcontrol").value) / 100);
+            if (parseFloat(document.querySelector("#tintfillcontrol").value) > 100) {
+                document.querySelector("#tintlayerXtra").setAttribute("fill-opacity", parseFloat(document.querySelector("#tintfillcontrol").value) / 100 - 1);
+                document.querySelector("#tintlayer").setAttribute("fill-opacity", "1");
+            }
+            else {
+                document.querySelector("#tintlayerXtra").setAttribute("fill-opacity", "0");
+            }
+
+            newPreview();
+
+        });
+        fR.readAsText(file);
+    });
+    fileUpload.click();
+});
