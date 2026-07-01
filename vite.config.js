9e0319fb3d126089536fbbfc64073df8ab83304f
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2026-07-01 11:17:25 +0800

    Set up electron app
    good

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
+export default defineConfig(({ mode }) => {
+  // Load all env variables re[gardless of prefix (3rd arg '')
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  // Set output directory based on env var, fallback to 'dist'
+  let outDir = 'dist';
+  if (env.VITE_PLATFORM === 'electron') {
+      outDir = 'electron-app/app';
+  }
+
+  return {
+    plugins: [svelte()],
+    base: "./",
+    build: { outDir },
+  }
 })
